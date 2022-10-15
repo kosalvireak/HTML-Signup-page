68fe71326bc0040f33bd66e531410c228551683c
Kosalvireak Khoeun <virakvary@gmail.com>
2022-10-16 06:37:31 +0700

    commit

--- connect.php
+++ connect.php
@@ -2,15 +2,15 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-
-    $conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','test');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(email, password) values(?, ?)");
-		$stmt->bind_param("ss",$email, $password);
-		$stmt -> $execute();
+		$stmt = $conn->prepare("insert into registration(Email, Password) values(?, ?)");
+		$stmt->bind_param("ss",$Email, $Password);
+		$execval = $stmt->execute();
+		echo $execval;
 		echo "Registration successfully...";
 		$stmt->close();
 		$conn->close();
