5c214b5810b2cc7a29c5b4dcdc7499865118fd49
Kosalvireak Khoeun <virakvary@gmail.com>
2023-02-06 22:53:11 +0700

    just approve change

--- connect.php
+++ connect.php
@@ -1,33 +1,17 @@
 <?php
 
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-<<<<<<< HEAD
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-    $conn = new mysqli('localhost','root','','test');
-=======
-	$conn = new mysqli('localhost','root','','test');
->>>>>>> 68fe71326bc0040f33bd66e531410c228551683c
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-<<<<<<< HEAD
-		$stmt = $conn->prepare("insert into registration(email, password) values(?, ?)");
-		$stmt->bind_param("ss",$email, $password);
-		$stmt -> $execute();
-=======
-		$stmt = $conn->prepare("insert into registration(Email, Password) values(?, ?)");
-		$stmt->bind_param("ss",$Email, $Password);
-		$execval = $stmt->execute();
-		echo $execval;
->>>>>>> 68fe71326bc0040f33bd66e531410c228551683c
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
-<<<<<<< HEAD
-?>
-=======
-?>
->>>>>>> 68fe71326bc0040f33bd66e531410c228551683c
+$conn = new mysqli('localhost', 'root', '', 'test');
+if ($conn->connect_error) {
+	echo "$conn->connect_error";
+	die("Connection Failed : " . $conn->connect_error);
+} else {
+	$stmt = $conn->prepare("insert into registration(email, password) values(?, ?)");
+	$stmt->bind_param("ss", $email, $password);
+	$stmt->$execute();
+	echo "Registration successfully...";
+	$stmt->close();
+	$conn->close();
+}
