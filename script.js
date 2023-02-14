59dadcceb6d4f19ec0ffac3d7b5e87ab160fac9c
Kosalvireak Khoeun <virakvary@gmail.com>
2023-02-14 15:03:32 +0700

    commit

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ var loadFile = function (event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
 }
+const vireak = Document.getElementById('h1')
